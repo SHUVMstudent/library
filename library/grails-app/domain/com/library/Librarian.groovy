adbfb9352968ab142054097592344acf353cd2ba
SHUVMstudent <b4006648@my.shu.ac.uk>
2016-03-15 12:06:05 +0000

    Created develop branch. Moved added library to the new branch. In process of creating controller class for Librarian.

---
+++ library/grails-app/domain/com/library/Librarian.groovy
@@ -0,0 +1,17 @@
+package com.library
+
+class Librarian {
+
+String name
+String email
+String userName
+String password
+String telephone
+
+String toString(){
+"$name,$userName"
+}
+
+    static constraints = {
+    }
+}
---
+++ library/src/test/groovy/library/ComControllerSpec.groovy
@@ -0,0 +1,22 @@
+package library
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(ComController)
+class ComControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
